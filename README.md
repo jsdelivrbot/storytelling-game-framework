21ff45c9db7c38e500dda68b68b931bd9d3a62fc
Joey Stevens <jstevens@egovstrategies.com>
2017-07-23 20:19:10 -0400

    add modes

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ The game is made up of scenes which are defined in JSON. A scene has an image, s
   "scene": 4,
   "image": "image-03.jpeg",
   "text": "There is a fork in the road which way do you go?",
+  "mode": "non-linear",
   "options":
   [
     {
@@ -21,6 +22,25 @@ The game is made up of scenes which are defined in JSON. A scene has an image, s
       "scene": 6
     }
   ]
+}
+```  
+
+## Modes  
+Each scene can have a mode. A mode defines how the scene is displayed:
+*non-linear*: Expects options to exist and will wait for a selection.  
+*auto*: Displays the text and waits for the config.wait setting (default 2000ms).  
+*linear*: Displays the text and will wait for a the next scene button to be pressed.
+See data/scene.json for more details  
+
+## config
+Inside of scene.json can be a config that is defined with the following properties:
+*wait*: Defines the wait time in ms of the auto mode.
+*mode*: Defines the starting/default mode. Note: Once a mode is set in a scene this is not used so it's best practice to not rely on this unless the entire game is in single mode.
+```
+"config":
+{
+    "mode": "non-linear",
+    "wait": 2000
 }
 ```  
 See data/scene.json for more details  
--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 var defaultTextSpeed = 70;
 var haltShowText = false;
+var autoModeOff;
 // Displays text at speed
 var showText = function (target, message, index, interval, override) {
   if (haltShowText && !override)
@@ -8,20 +9,34 @@ var showText = function (target, message, index, interval, override) {
     return;
   }
 
-  if (index < message.length) {
+  if (index < message.length)
+  {
     $(target).append(message[index++]);
     setTimeout(function () { showText(target, message, index, interval, override); }, interval);
+  }
+  else
+  {
+    finishedDisplayingText = true;
   }
 }
 
 // Creates a new scene
 var newScene = function (text, image, sceneId) {
   setCurrentScene(sceneId);
+  // Override the default mode if needed.
+  checkMode(sceneId);
+
+  // Prep the scene
   clearMsgBox();
+  // Override prevents text from displaying in new scene if a selection is made before it finishes displaying
   var override;
   haltShowText ? override = true :  override = false;
   showText('#msg', text, 0, defaultTextSpeed, override);
+
+  // Display the image
   document.querySelector("img.scene").src = 'img/' + image;
+
+  // Display options if any
   displayOptions(sceneId);
 }
 
@@ -52,7 +67,6 @@ var nextScene = function() {
   var currentScene = getCurrentScene();
   var sceneText = lookup[currentScene+1].text;
   var sceneImage = lookup[currentScene+1].image;
-
   newScene(sceneText, sceneImage, currentScene+1);
 }
 
@@ -77,27 +91,74 @@ var loadScenes = function () {
   var sceneImage = lookup[startScene].image;
   newScene(sceneText, sceneImage, startScene);
 }
+var turnOnLinearMode = function() {
+  var span = document.getElementsByClassName("close")[0];
+  // When the user clicks on <span> (x), goto the next scene
+  span.style.display="";
+  span.onclick = function()
+  {
+      nextScene();
+  }
+};
+var checkMode = function(sceneId) {
+  if(lookup[sceneId].mode)
+  {
+    setMode(lookup[sceneId].mode);
+  }
+};
+
+var turnOnAutoMode = function() {
+  Object.defineProperty(this, 'finishedDisplayingText', {
+    configurable: true,
+    get: function () { return myVar; },
+    set: function (v) {
+      myVar = v;
+      if (autoModeOff == false)
+      {
+        setTimeout(function(){
+          nextScene();
+        }, config.wait);
+      }
+    }
+  });
+};
+
+// Set the mode
+// Auto = Display text and go to the nextScene
+// non-linear = Display text + Options and wait
+// linear = Display text + show a nextScene arrow and wait
+var setMode = function(mode) {
+  var span = document.getElementsByClassName("close")[0];
+  console.log(mode);
+  if(mode == "linear")
+  {
+    autoModeOff = true;
+    config.mode = mode;
+    turnOnLinearMode();
+  }
+  else if (mode == "non-linear")
+  {
+    autoModeOff = true;
+    span.style.display="none";
+  }
+  else if (mode == "auto")
+  {
+    span.style.display="none";
+    autoModeOff = false;
+    turnOnAutoMode();
+  }
+  else
+  {
+      console.log("Invalid mode detected: " + mode);
+  }
+}
 
 window.addEventListener("load", function load(event){
+  config.mode = config.mode || "linear";
+  config.wait = config.wait || 2000;
   if(document.readyState === 'complete') {
     $("#options").hide();
     loadScenes();
-
-    // Get the modal
-    var modal = document.getElementById('myModal');
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        nextScene();
-    }
-
-
 
   }
 });
